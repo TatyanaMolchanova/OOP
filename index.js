de7eea883f0fc708cfbe0b334a8a1db5cd2199f2
Molchanova Tatyana <mtuspeh@gmail.com>
2021-04-26 20:03:22 +0300

    oop

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
-console.log('Hello, my Dear, You are beloved, by myself')
+// // simple Object
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         Y: 1
+//     },
+//     draw: function() {
+//         console.log('draw')
+//     }
+// };
 
-// 11:12 https://www.youtube.com/watch?v=PFmuCDHHpwk
+// // Factory Function
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw: function() {
+//             console.log('draw')
+//         }
+//     };
+// }
+
+// const circle = createCircle(1)
+// // circle.draw();
+
+// // Constructor Function
+// function Circle(radius) {
+//     console.log('this', this)
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw')
+//     }
+// }
+
+// // Circle.
+
+// // const Circle1 = new Function('radius', `
+// //     this.radius = radius;
+// //     this.draw = function() {
+// //         console.log('draw')
+// //     }
+// // `)
+
+// // const circle = new Circle1(1)
+
+// Circle.call({}, 1) // it's the same as // const another = new Circle(1)
+// Circle.apply({}, [1])
+
+// const another = new Circle(1)
+// const another = Circle(1)
+
+// let x = {};
+
+// // let x = new Object()
+// new String(); // '', "", ``
+// new Boolean(); // true, false
+// new Number(); // 1, 2, 3
+
+// Primitives
+// let x = 10
+// let y = x
+
+// x = 20
+
+// console.log('y', y) // y == 10
+
+// Reference types
+// let x = { value: 10 }
+// let y = x
+
+// x.value = 20
+
+// console.log('y', y) // y { value: 20 }
+
+//Primitives
+
+// let number = 10
+
+// function increase(number) {
+//     number++
+// }
+
+// increase(number)
+// console.log(number) //10
+
+let number = {value: 10}
+
+function increase(number) {
+    number++
+}
+
+increase(number)
+console.log(number) //10
+
+//35.46 https://www.youtube.com/watch?v=PFmuCDHHpwk
